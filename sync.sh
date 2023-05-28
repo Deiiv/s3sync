4818eb899598264d58354504f72162d4ab14bbd5
David Fedorovsky <david.fedorovsky@gmail.com>
2023-05-28 10:13:46 -0400

    ignoring thumbnails

--- sync.sh
+++ sync.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "$(date) - Starting sync job with command aws s3 sync /data s3://$BUCKET$BUCKET_PATH" --exclude ".ppignore" --exclude "#recycle/*" --exclude "*/@eaDir/*" --exclude "*/WhatsApp Voice Notes/*" --exclude "*/WhatsApp Animated Gifs/*" --exclude "*/WhatsApp Audio/*" --exclude "*/WhatsApp Stickers/*" --exclude "*/WhatsApp Documents/*" --exclude "*/Sent/*"
+echo "$(date) - Starting sync job with command aws s3 sync /data s3://$BUCKET$BUCKET_PATH" --exclude ".ppignore" --exclude "#recycle/*" --exclude "*/@eaDir/*" --exclude "*/WhatsApp Voice Notes/*" --exclude "*/WhatsApp Animated Gifs/*" --exclude "*/WhatsApp Audio/*" --exclude "*/WhatsApp Stickers/*" --exclude "*/WhatsApp Documents/*" --exclude "*/Sent/*" --exclude "*/.thumbnails/*"
 
-aws s3 sync /data s3://"$BUCKET""$BUCKET_PATH" --exclude ".ppignore" --exclude "#recycle/*" --exclude "*/@eaDir/*" --exclude "*/WhatsApp Voice Notes/*" --exclude "*/WhatsApp Animated Gifs/*" --exclude "*/WhatsApp Audio/*" --exclude "*/WhatsApp Stickers/*" --exclude "*/WhatsApp Documents/*" --exclude "*/Sent/*" && mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PW -t $MQTT_TOPIC -m "Healthy as of $(date '+%d-%m-%Y')"
+aws s3 sync /data s3://"$BUCKET""$BUCKET_PATH" --exclude ".ppignore" --exclude "#recycle/*" --exclude "*/@eaDir/*" --exclude "*/WhatsApp Voice Notes/*" --exclude "*/WhatsApp Animated Gifs/*" --exclude "*/WhatsApp Audio/*" --exclude "*/WhatsApp Stickers/*" --exclude "*/WhatsApp Documents/*" --exclude "*/Sent/*" --exclude "*/.thumbnails/*" && mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PW -t $MQTT_TOPIC -m "Healthy as of $(date '+%d-%m-%Y')"
 
 echo "$(date) - End of sync job"
